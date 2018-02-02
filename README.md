9aa1fe1ffb467fcb14aa449fca5ce3559971483b
StudentWan <benyuwan@gmail.com>
2018-02-02 17:26:30 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+### 介绍
+
+本仓库主要用于存放自己在学习Vue实现原理时所写的一些代码和注释。
+
+由于时间原因和一些其他因素，没有直接全面的阅读Vue源代码，而是借助一些开发者的探索和Vue源代码一起来进行学习。
+
+代码或注释中有不正确的地方欢迎PR。
+
+有疑问的地方欢迎ISSUE。
+
+### 参考资料：
+
+[Vue](https://github.com/vuejs/vue)
+
+[梁少峰的博客](https://github.com/youngwind)
+
+[Vue-Analysis](https://github.com/Ma63d/vue-analysis)
+
+[Vue2.1.7源码学习](http://hcysun.me/2017/03/03/Vue%E6%BA%90%E7%A0%81%E5%AD%A6%E4%B9%A0/)
---
+++ simple-object-watch/vue.js
@@ -0,0 +1,46 @@
+// Observer构造函数
+// Observer构造函数为参数对象设置访问器属性，以实现数据观察
+function Observer(data) {
+    this.data = data
+    this.walk(data)
+}
+
+const proto = Observer.prototype
+
+proto.walk = function (data) {
+    let val
+    for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+            // 以对象属性的初始值设置val
+            val = data[key]
+            if (typeof val === 'object') {
+                // 如果val仍是对象，以val为参数调用Observer构造函数
+                // 该构造函数调用为val对象中的属性设置getter和setter
+                new Observer(val)
+            }
+            else {
+                // 调用convert函数，将对对象数据属性的访问转换为对访问器属性的访问
+                this.convert(val, key)
+            }
+        }
+    }
+}
+
+proto.convert = function (val, key) {
+    // 访问器实际上访问和修改的值是闭包val，仅仅用对象数据属性进行初始化
+    Object.defineProperty(this.data, key, {
+        configurable: true,
+        enumerable: true,
+        get: function () {
+            console.log('你访问了：' + key)
+            return val
+        },
+        set: function (v) {
+            console.log('你设置了：' + key)
+            console.log(key + ' = ' + v)
+            if (v !== val) {
+                val = v
+            }
+        }
+    })
+}
