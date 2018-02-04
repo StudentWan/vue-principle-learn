6ec05e936d465844b93b411ee61b2f19e7c8f14d
StudentWan <benyuwan@gmail.com>
2018-02-04 17:05:50 +0800

    add: object deepbinding and watch && fix: first implement bug

--- object-deep-binding-and-watch/vue.js
+++ object-deep-binding-and-watch/vue.js
@@ -2,6 +2,7 @@
 // Observer构造函数为参数对象设置访问器属性，以实现数据观察
 function Observer(data) {
     this.data = data
+    this.handlers = {}
     this.walk(data)
 }
 
@@ -18,15 +19,15 @@ proto.walk = function (data) {
                 // 该构造函数调用为val对象中的属性设置getter和setter
                 new Observer(val)
             }
-            else {
-                // 调用convert函数，将对对象数据属性的访问转换为对访问器属性的访问
-                this.convert(val, key)
-            }
+
+            // 调用convert函数，将对对象数据属性的访问转换为对访问器属性的访问
+            this.convert(val, key)
         }
     }
 }
 
 proto.convert = function (val, key) {
+    const self = this
     // 访问器实际上访问和修改的值是闭包val，仅仅用对象数据属性进行初始化
     Object.defineProperty(this.data, key, {
         configurable: true,
@@ -38,9 +39,36 @@ proto.convert = function (val, key) {
         set: function (v) {
             console.log('你设置了：' + key)
             console.log(key + ' = ' + v)
+
+            if (typeof v === 'object') {
+                new Observer(v)
+            }
+
             if (v !== val) {
                 val = v
             }
+
+            self.$emit(key, v)
         }
     })
 }
+
+proto.$watch = function (evtType, handler) {
+    // 订阅事件
+    if (!(evtType in this.handlers)) {
+        this.handlers[evtType] = []
+    }
+    this.handlers[evtType].push(handler)
+    return this
+}
+
+proto.$emit = function (evtType, ...args) {
+    // 发布事件
+    // 判断条件的用处在于防止没有观察者时报错
+    if (Object.prototype.toString.call(this.handlers[evtType]) === '[object Array]') {
+        for (const handler of this.handlers[evtType]) {
+            handler.apply(this, args)
+        }
+    }
+}
+
